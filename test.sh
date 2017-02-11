7d97b683900870d093228839570848853b1f3611
Esteban J Fallas <ejf@ST1.local>
2017-02-10 20:31:54 -0600

    new branch 1st-feat created

--- test.sh
+++ test.sh
@@ -16,3 +16,6 @@ echo "Git works with SSH keys, just generate a new key file,\n
 	add it to your local ssh-agent with 'ssh-add FILENAME.pub',\n
 	and finally reset your remote at the local machine,\n
 	git remote set-url origin git@github.com:USERNAME/REPO.git"
+
+echo "Git allows also for branch creation and handling several steps while doing so,\n
+just run 'git branch NAME', followed by 'git checkout NAME' to switch to that branch"
